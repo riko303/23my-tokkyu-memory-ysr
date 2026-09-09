bdaebc8264dc98b5e55406282f53f032a6e228ea
riko303 <riko_line_303@yahoo.co.jp>
2026-09-10 00:39:40 +0900

    videos.js を作成

---
+++ videos.js
@@ -0,0 +1,22 @@
+// 動画データをここにまとめて管理します
+const videoData = [
+  {
+    title: 『Believe×Believe』from BULLET TRAIN ARENA TOUR 2025-2026「REAL?」,
+    desc: ほぼリョウガとマサヒロしか映ってない,
+    thumb: "photo-output.png",
+    time: "04:36",
+    isNew: true, // NEWマークをつける場合は true、つけない場合は false
+    category: "power", // ジャンル
+    driveId: "1yhIwDsNCvOJ-k5aZzXlZeVM8qofwucTH"
+  },
+  {
+    title: 『超えてアバンチュール』from BULLET TRAIN ARENA TOUR 2025-2026「REAL?」,
+    desc: 全員の変顔に注目！,
+    thumb: "IMG_5294.jpeg",
+    time: "03:48",
+    isNew: true,
+    category: "power",
+    driveId: "1U9sf85PlSCW3_COvKLORZYlD6r1wrw8T"
+  },
+
+];
