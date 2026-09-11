4607cf4b7e1c015985f67d3f02b6ee6751406e9f
riko303 <riko_line_303@yahoo.co.jp>
2026-09-11 20:50:53 +0900

    videos.js を更新

--- videos.js
+++ videos.js
@@ -51,7 +51,7 @@ const videoData = [
   },
   
     {
-    title: "『原因は自分にある』from EBiDAN THE LIVE 2025 HOTEL NINE STAR 【Day1】Emerald GATE ",
+    title: "『原因は自分にある。』from EBiDAN THE LIVE 2025 HOTEL NINE STAR 【Day1】Emerald GATE ",
     desc: "シャッフルでしか見られない歌ってるレアリョウガ！メロリョウガに注目！",
     thumb: "IMG_5343.jpeg", //
     time: "03:10",
